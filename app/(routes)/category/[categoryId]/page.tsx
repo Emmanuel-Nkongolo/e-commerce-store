cc455ad22d1dbf9e3ec9649bbb667bb925971ca9
Emmanuel nkongolo <nkongolo64@gmail.com>
2023-10-26 23:46:15 +0200

    create toast provider, the preview modal and rendering the sizes with mobile development

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -1,12 +1,16 @@
-import getCategory from "@/actions/get-category";
-import getColors from "@/actions/get-colors";
-import getProducts from "@/actions/get-products";
-import getSizes from "@/actions/get-sizes";
-import Billboard from "@/components/billboard";
+
 import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import ProductCard from "@/components/ui/product-card";
 import NoResults from "@/components/ui/no-results";
 
+import getProducts from "@/actions/get-products";
+import getCategory from "@/actions/get-category";
+import getSizes from "@/actions/get-sizes";
+import getColors from "@/actions/get-colors";
+
 import Filter from "./components/filter";
+import MobileFilters from "./components/mobile-filters";
 
 export const revalidate = 0
 
@@ -40,22 +44,31 @@ const CategoryPage: React.FC<CategoryPageProps> = async ({
                     data={category.billboard}
                 />
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
-                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8"></div>
-                    {/* Add Mobile Filters */}
-                    <div className="hidden lg:block">
-                        <Filter
-                            valueKey="sizeId"
-                            name="Sizes"
-                            data={sizes}
-                        />
-                        <Filter
-                            valueKey="colorId"
-                            name="Colors"
-                            data={colors}
-                        />
-                    </div>
-                    <div className="mt-6 lg:col-span-4 lg:mt-0">
-                        {products.length === 0 && <NoResults />}
+                    <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
+                        <MobileFilters sizes={sizes} colors={colors} />
+                        <div className="hidden lg:block">
+                            <Filter
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
+                            />
+                            <Filter
+                                valueKey="colorId"
+                                name="Colors"
+                                data={colors}
+                            />
+                        </div>
+                        <div className="mt-6 lg:col-span-4 lg:mt-0">
+                            {products.length === 0 && <NoResults />}
+                            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                                {products.map((item) => (
+                                    <ProductCard
+                                        key={item.id}
+                                        data={item}
+                                    />
+                                ))}
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Container>
